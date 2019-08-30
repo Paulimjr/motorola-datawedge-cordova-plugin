ad8f480a1a1f5a982dc34d4b52baefd322104335
Paulimjr <paulim.jr@hotmail.com>
2019-08-30 11:51:15 +0100

    Remove methods unsed on datawedge.js

--- www/datawedge.js
+++ www/datawedge.js
@@ -1,51 +1,48 @@
-cordova.define("com.bluefletch.motorola.MotorolaDataWedge", function (require, exports, module) {
-    var cordova = require('cordova');
-    var exec = require('cordova/exec');
+var cordova = require('cordova');
+var exec = require('cordova/exec');
 
-    /**
-            * Constructor.
-    *
-    * @returns {DataWedge}
-    */
-    function DataWedge() {
+/**
+        * Constructor.
+*
+* @returns {DataWedge}
+*/
+function DataWedge() {
 
-    };
+};
 
-    /**
-     * Turn on DataWedge (default profile) and listen for event.  Listens for hardward button events.
-     * 
-     * @param successCallback - Success function should expect a barcode to be passed in
-     * @param intentAction - action to listen for.  This is what you configured in the DataWedge app.  
-     *       Defaults to: "com.bluefletch.motorola.datawedge.ACTION";
-     */
-    DataWedge.prototype.start = function (intentAction) {
-        var args = [];
-        if (intentAction) {
-            args[0] = intentAction;
-        }
-        exec(null, null, 'MotorolaDataWedge', 'start', args);
-    };
+/**
+ * Turn on DataWedge (default profile) and listen for event.  Listens for hardward button events.
+ * 
+ * @param successCallback - Success function should expect a barcode to be passed in
+ * @param intentAction - action to listen for.  This is what you configured in the DataWedge app.  
+ *       Defaults to: "com.bluefletch.motorola.datawedge.ACTION";
+ */
+DataWedge.prototype.start = function (intentAction) {
+    var args = [];
+    if (intentAction) {
+        args[0] = intentAction;
+    }
+    exec(null, null, 'MotorolaDataWedge', 'start', args);
+};
 
-    /**
-     * Turn off DataWedge plugin
-     */
-    DataWedge.prototype.stop = function () {
+/**
+ * Turn off DataWedge plugin
+ */
+DataWedge.prototype.stopScanner = function () {
 
-        exec(null, null, 'MotorolaDataWedge', 'stop', []);
-    };
-
-
-    /**
-     * Register a callback for scan events.  This function will be called when barcdoes are read
-     */
-    DataWedge.prototype.registerForBarcode = function (callback) {
-
-        exec(callback, null, 'MotorolaDataWedge', 'scanner.register', []);
-    };
+    exec(null, null, 'MotorolaDataWedge', 'stop', []);
+};
 
 
-    //create instance
-    var DataWedge = new DataWedge();
+/**
+ * Register a callback for scan events.  This function will be called when barcdoes are read
+ */
+DataWedge.prototype.registerForBarcode = function (callback) {
 
-    module.exports = DataWedge;
-});
+    exec(callback, null, 'MotorolaDataWedge', 'scanner.register', []);
+};
+
+//create instance
+var DataWedge = new DataWedge();
+
+module.exports = DataWedge;
